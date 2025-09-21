51517b3af352205b152713cb3a3b3439065f7b1a
Harshcodesfast <harshawalegaonkar@gmail.com>
2025-09-21 21:13:57 +0530

    done

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import HeroSection from "./components/HeroSection";
+import HowItWorks from "./components/HowItWorks";
+import Navbar from "./components/Navbar";
+import KeyFeatures from "./components/KeyFeatures";
+import PricingPlans from "./components/PricingPlans";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <main className="text-sm text-neutral-300 antialiased">
+        <Navbar />
+        <HeroSection />
+        <HowItWorks />
+        <KeyFeatures />
+        <PricingPlans />
+        <Testimonials />
+        <Footer />
+      </main>
+    </>
+  );
+}
+
+export default App;
